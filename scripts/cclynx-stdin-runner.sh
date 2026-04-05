49cc864b911c1eafe26d9c26d7dcf1c1c050733c
Denis Korchagin <denis.korchagin.1995@gmail.com>
2026-04-05 21:49:07 +0200

    test: updates tests followed by new features of jcunit

--- scripts/cclynx-stdin-runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper to pipe file content via stdin to cclynx.
-# jcunit passes: <filepath> <args...>
-# We pipe the file via stdin and pass remaining args to cclynx.
-filepath="$1"
-shift
-cat "$filepath" | ./bin/cclynx "$@" /dev/stdin
